814c37c780a5b5fbfd58436c69f92398bbb5519d
Damien Gounot <gounot@et.esiea.fr>
2019-10-20 16:15:34 +0200

    Bank Banner
    + Adding Banner with images related to bank Industry

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,13 @@
 
 <template>
+<div>
+  <v-carousel >
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
   <v-form>
            <v-alert
       v-model="show"
@@ -38,6 +46,7 @@
       Register
     </v-btn>
   </v-form>
+  </div>
 </template>
 
 <script>
@@ -48,7 +57,21 @@ export default {
     show: false,
     msgType: 'error',
     statusMsg: '',
-    loginGranted: false
+    loginGranted: false,
+    items: [
+      {
+        src: 'https://www.swipez.in/uploads/images/landing/rQ0busDtPobLZYjPK3jrHxryWiKBxUqBxk9Ij0vAs94.jpg'
+      },
+      {
+        src: 'https://www.oceanbankonline.com/images/businessonlinebanking_banner.jpg'
+      },
+      {
+        src: 'https://dhakabankltd.com/wp-content/uploads/2012/12/debit-card-banner-img.jpg'
+      },
+      {
+        src: 'https://www.bromford.co.uk/media/3731/main-page-banner.jpg'
+      }
+    ]
   }),
 
   methods: {
